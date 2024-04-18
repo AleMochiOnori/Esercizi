00b419531ca9da7f764562c2167074e1b86fc4c0
AlemochiOnori <alessandromochionori@gmail.com>
2024-04-18 12:27:45 +0200

    esercizi fino al 4

---
+++ lezione2/Lezione2.py
@@ -0,0 +1,35 @@
+#Alessandro mochi onori 18/04/2024
+
+print("Hello world")
+
+
+#Esercizio 1
+
+Eric: str = "Eric"
+
+ericMessage: str =   'do you want to learn some Python today?'
+
+
+print(" Hey " , Eric , ericMessage)
+
+
+#Esercizio 2
+
+flavio:str = "Flavio"
+
+
+print(flavio.lower(),flavio.upper(),flavio.title())
+
+#Esercizio 3
+
+obiwan: str = "Obiwan said to general Gravious :"
+phrase:str = "Hello there"
+
+
+print(f"{obiwan}{phrase}")
+
+
+
+#Esercizio 4
+
+
