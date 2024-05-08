326a8b8106611569ea99474f2bee40536d448f56
AlessandroMochiOnori <alessandromochionori@gmail.com>
2024-05-08 12:53:36 +0200

    ex6 lezione 6

--- lezione6/lezione6 esercizi.py
+++ lezione6/lezione6 esercizi.py
@@ -139,3 +139,34 @@ Roberta.increment_login_attempts(3)
 Roberta.logging_attempts2()
 Roberta.reset_login_attempts()
 Roberta.logging_attempts2()
+
+
+print()
+
+
+
+# 9-6. Ice Cream Stand: An ice cream stand is a specific kind of restaurant. Write a class called IceCreamStand that inherits from the Restaurant class you wrote in Exercise 9-1  or Exercise 9-4. Either version of the class will work; just pick the one you like better. Add an attribute called flavors that stores a list of ice cream flavors. Write a method that displays these flavors. Create an instance of IceCreamStand, and call this method. 
+
+
+
+
+class IceCreamStand :
+    def __init__(self,restaurant_name,cuisine_type, flavors) :
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+        self.flavors = flavors
+
+
+    def display_flavors(self):
+        print(self.flavors)
+
+
+
+
+
+iceCream = IceCreamStand("The best ice cream","Ice Cream",["Chocolate", "Vanilla", "Strawberry"])
+
+iceCream.display_flavors()
+
+
+print()
